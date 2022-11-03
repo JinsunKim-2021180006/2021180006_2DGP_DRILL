e9c39a5cab24bcbcf0de7ba3c5171670efa9b33b
JinsunKim-2021180006 <112993350+JinsunKim-2021180006@users.noreply.github.com>
2022-11-03 16:49:27 +0900

    drill 12 완성
    2021180006 김진선

---
+++ drill/drill 12/grass.py
@@ -0,0 +1,16 @@
+from pico2d import *
+
+class Grass:
+    image = None
+    def __init__(self,x,y):
+        if Grass.image == None:
+            self.image = load_image('grass.png')
+        self.x,self.y =x,y
+    
+    def update(self):
+        pass
+    
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
